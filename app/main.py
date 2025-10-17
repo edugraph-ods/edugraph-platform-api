c71c6d032dd8fb404247277961d4650d52a07a94
equinox-1092 <sdiaz1234di@gmail.com>
2025-10-17 16:45:52 -0500

    feat(user): implement user repository and auth flows

--- app/main.py
+++ app/main.py
@@ -2,7 +2,23 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from app.api.v1.routers.auth_router import router as auth_router
+from app.api.v1.routers.protected_router import router as protected_router
+from app.adapters.db.models import Base
+from app.adapters.db.session import create_database_if_not_exists, engine
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def on_startup():
+    await create_database_if_not_exists()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+app.include_router(auth_router)
+app.include_router(protected_router)
 
 
 @app.get("/")
