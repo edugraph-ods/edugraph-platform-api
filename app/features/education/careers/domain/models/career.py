17f5a1c722f9ab9546c30892cb669caf8d4853ff
Far14z <faridce14@gmail.com>
2025-11-19 00:15:16 -0500

    feat(careers): add career entity.

---
+++ app/features/education/careers/domain/models/career.py
@@ -0,0 +1,21 @@
+﻿import uuid
+from dataclasses import dataclass, field
+
+
+def _generate_object_id() -> str:
+    return uuid.uuid4().hex[:24]
+@dataclass
+class Career:
+    name: str
+    program: str
+    university_id: str
+
+    id: str = field(default_factory=_generate_object_id)
+
+    @classmethod
+    def create(cls, name: str, program: str, university_id: str):
+        return cls(
+            name=name,
+            program=program,
+            university_id=university_id,
+        )
