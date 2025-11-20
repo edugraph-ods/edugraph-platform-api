071d9c4a3d32a0801af601e50ec59dbcf5ef27bb
Far14z <faridce14@gmail.com>
2025-11-20 17:03:07 -0500

    feat(courses): enhance academic progress response and update course retrieval to include prerequisites.

--- app/features/education/courses/application/internal/inbound_services/use_cases/get_course_by_id_use_case.py
+++ app/features/education/courses/application/internal/inbound_services/use_cases/get_course_by_id_use_case.py
@@ -5,19 +5,27 @@ from app.features.education.courses.domain.repositories.course_repository import
 
 from fastapi import HTTPException
 
+from app.features.education.courses.interfaces.rest.schemas.course_reponse import CourseResponse
+
+
 class GetCourseByIdUseCase:
     def __init__(self, repository: CourseRepository):
         self.repository = repository
 
     async def execute(self, course_id: str):
-        print(f"[DEBUG] Buscando curso con ID: {course_id}")  # debug
 
-        course = await self.repository.find_by_id(course_id)
-
-        if not course:
-            print(f"[DEBUG] Curso no encontrado para ID: {course_id}")  # debug
+        result = await self.repository.find_by_id(course_id)
+        if not result:
             raise HTTPException(status_code=404, detail="Course not found")
 
-        print(f"[DEBUG] Curso encontrado: {course}")  # debug
-        return course
+        course, prereq_names = result
+
+        return CourseResponse(
+            id=course.id,
+            name=course.name,
+            code=course.code,
+            cycle=course.cycle,
+            credits=course.credits,
+            prerequisite=prereq_names
+        )
 
