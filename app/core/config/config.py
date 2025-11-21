939a720a721acb8a070bdabe7f3622d6ba0954f7
equinox-1092 <sdiaz1234di@gmail.com>
2025-11-20 20:59:24 -0500

    feat(academic-progress): optimize cycle planning under credit cap

--- app/core/config/config.py
+++ app/core/config/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     
     # Database -mysql
     username: str = "root"
-    password: str = "Hawaiian2014"
+    password: str = "equinox1092"
     host: str = "localhost"
     port: int = 3306
     database: str = "edugraph"
@@ -28,7 +28,7 @@ class Settings(BaseSettings):
     smtp_sender: str | None = None
 
     # CORS ORIGINS
-    backend_cors_origins: List[str] = ["http://localhost:3000"]
+    backend_cors_origins: List[str] = ["http://localhost:3000", "http://192.168.18.213:3000"]
     
     class Config:
         env_file = ".env"
